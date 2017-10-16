e61a2df629160d355a25d1318b83d93417aece53
NicolasB <boullet.nicolas@gmail.com>
2017-10-16 12:09:22 +0200

    Première mise en place du projet et formattage du code

--- pokedeck/src/main/java/upmc/pcg/ui/GameUI.java
+++ pokedeck/src/main/java/upmc/pcg/ui/GameUI.java
@@ -1,37 +1,32 @@
-// Copyright 2017 Pierre Talbot (IRCAM)
-
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-
-//     http://www.apache.org/licenses/LICENSE-2.0
-
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// School project : Pokedeck
+//
+// Write by Nicolas BOULLET and Gabin FIQUET
+// Studying at UPMC in Licence Pro Projet Web (2017/2018)
 
 package upmc.pcg.ui;
 
 import java.util.*;
 import upmc.pcg.game.Game;
 
+/*
+ * GameUI
+ */
 public class GameUI {
-  private final Game game = new Game();
-  private final Scanner console = new Scanner(System.in);
+    private final Game game = new Game();
+    private final Scanner console = new Scanner(System.in);
 
-  public void start() {
-    print_welcome_msg();
-    ArrayList<String> names = ask_players_names();
-    game.initialize(names);
-    game.play();
-  }
+    public void start() {
+        print_welcome_msg();
+        ArrayList<String> names = ask_players_names();
+        game.initialize(names);
+        game.play();
+    }
 
-  private ArrayList<String> ask_players_names() {
-    return new ArrayList<>();
-  }
+    private ArrayList<String> ask_players_names() {
+        return new ArrayList<>();
+    }
 
-  private void print_welcome_msg() {
-  }
+    private void print_welcome_msg() {
+        //todo
+    }
 }
